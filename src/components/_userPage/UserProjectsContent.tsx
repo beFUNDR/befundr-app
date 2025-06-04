0886d9e9718f510df6bc45a477cdb95c824bc342
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2025-06-04 10:57:34 +0200

    page skillsHub/[userId] responsive OK

--- src/components/_userPage/UserProjectsContent.tsx
+++ src/components/_userPage/UserProjectsContent.tsx
@@ -5,19 +5,6 @@ import ProjectCard from "../cards/ProjectCard";
 import LoaderSmall from "../displayElements/LoaderSmall";
 import { useProjectsByUserId } from "@/hooks/dbData/project/useProject";
 
-const mockProjects = [
-  {
-    name: "beFUNDR",
-    description: "The decentralized incubator.",
-    image: "/images/projects/befundr_image.png",
-  },
-  {
-    name: "SolanaTools",
-    description: "A dashboard for Solana analytics.",
-    image: "/images/projects/befundr_image.png",
-  },
-];
-
 const UserProjectsContent = ({ userId }: { userId: string }) => {
   const { data: projects, isLoading, error } = useProjectsByUserId(userId);
 
@@ -25,7 +12,7 @@ const UserProjectsContent = ({ userId }: { userId: string }) => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-4 justify-items-center">
       {projects?.map((project, idx) => (
         <Link href={`/project/${project.id}`} key={idx}>
           <ProjectCard project={project.data} />
